5739069efed006d1e589fd85408f43d58cc9acfd
Sergio Blázquez Segovia <setibs@gmail.com>
2023-07-08 10:47:22 +0200

    Starting commit

--- src/swg/articles.py
+++ src/swg/articles.py
@@ -5,3 +5,9 @@ def get_article_metadata(article: str) -> dict:
     md = markdown.Markdown(extensions=["full_yaml_metadata"])
     md.convert(article)
     return md.Meta
+
+
+def get_article_from_path(path: str) -> str:
+    with open(path,"r") as art:
+        text = art.read()
+        return text
